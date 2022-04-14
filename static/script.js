467c4a419de72d2c3c6790f014dbcb4bc28230a2
naleen201 <57667084+naleen201@users.noreply.github.com>
2022-04-14 22:43:48 +0530

    UI Improvements

--- static/script.js
+++ static/script.js
@@ -1,22 +1,22 @@
 document.getElementById('inputfile')
-            .addEventListener('change', function() {
-              
-            var fr=new FileReader();
-            fr.onload=function(){
-                document.getElementById('input_textarea')
-                        .value=fr.result;
-            }
-              
-            fr.readAsText(this.files[0]);
-        })
+    .addEventListener('change', function() {
+
+        var fr = new FileReader();
+        fr.onload = function() {
+            document.getElementById('input_textarea')
+                .value = fr.result;
+        }
+
+        fr.readAsText(this.files[0]);
+    })
 
 function getSummary() {
     var input_text = document.getElementById("input_textarea").value;
     var summary_size = document.getElementById("summary_size").value;
     fetch('summarize?' + new URLSearchParams({
-        text: input_text,
-        required_summary_length: summary_size,
-    }))
+            text: input_text,
+            required_summary_length: summary_size,
+        }))
         .then(response => response.json())
         .then((data) => {
             document.getElementById("output_textarea").value = data["summary"];
@@ -41,10 +41,10 @@ function download(filename, text) {
 }
 
 // Start file download.
-document.getElementById("download_button").addEventListener("click", function(){
+document.getElementById("download_button").addEventListener("click", function() {
     // Generate download of hello.txt file with some content
     var text = document.getElementById("output_textarea").value;
     var filename = "Summary.txt";
-    if(text.length != 0)
+    if (text.length != 0)
         download(filename, text);
 }, false)
